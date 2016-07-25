7cf6009f9d4a2856375965c4dda80ffd98091b26
David Tudury <david.tudury@gmail.com>
2016-07-25 02:05:16 -0700

    fix horrible tabbing accident

--- Source/Main.hx
+++ Source/Main.hx
@@ -8,11 +8,11 @@ import display.ControlPanel;
 
 class Main extends Sprite {
 
-	public function new () {
-		super ();
+    public function new () {
+        super ();
         stage.frameRate = 60;
         addChild (new ReelBlurs(stage));
         addChild (new ControlPanel());
         addChild (new FPS());
-	}
+    }
 }
